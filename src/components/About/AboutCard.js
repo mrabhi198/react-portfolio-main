e3ef6705d508587d5ab19a2c606032c63168144d
Abhinav Alok <abhinavalok318@gmail.com>
2024-11-14 18:12:35 +0530

    fix logo, and info

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -8,33 +8,29 @@ function AboutCard() {
       <Card.Body>
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
-            Hi Everyone, I am <span className="purple">Ankita Sinha </span>
-            from <span className="purple"> Bihar, India.</span>
+            Hi Everyone, I'm <span className="purple">Ankita Sinha, </span>
+            hailing from <span className="purple"> Bihar, India.</span>
             <br />
-            I am currently pursuing my BBA from <span className="purple">Lovely Professional University.</span>
+            I'm currently pursuing a <span className="purple"> Bachelor of Business Administration</span> at <span className="purple">Lovely Professional University.</span>
+            <br/>
+            <br/>
+            Recently, I completed an enriching internship with <span className="purple">Jain Divya & Co.</span> where I gained hands-on experience in <span className="purple">financial auditing, compliance,</span> and 
+            <span className="purple"> report analysis</span> under the mentorship of a Chartered Accountant. This role fueled my passion for finance, and I'm eager to explore more opportunities in the field to deepen my expertise and make an impact!"
             <br />
-            I have completed Integrated MSc (IMSc) in Maths and Computing at BIT
-            Mesra.
             <br />
-            <br />
-            Apart from coding, some other activities that I love to do!
+            Apart from these, some other activities that I love to do!
           </p>
           <ul>
             <li className="about-activity">
-              <ImPointRight /> Playing Games
+              <ImPointRight /> Sleeping
             </li>
             <li className="about-activity">
-              <ImPointRight /> Writing Tech Blogs
+              <ImPointRight /> Making Blogs
             </li>
             <li className="about-activity">
               <ImPointRight /> Travelling
             </li>
           </ul>
-
-          <p style={{ color: "rgb(155 126 172)" }}>
-            "Strive to build things that make a difference!"{" "}
-          </p>
-          <footer className="blockquote-footer">Soumyajit</footer>
         </blockquote>
       </Card.Body>
     </Card>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,8 @@
-import React, { useState, useEffect } from "react";
-import Navbar from "react-bootstrap/Navbar";
-import Nav from "react-bootstrap/Nav";
-import Container from "react-bootstrap/Container";
-import logo from "../Assets/logo.png";
-import { Link } from "react-router-dom";
-import { AiOutlineHome, AiOutlineFundProjectionScreen, AiOutlineUser } from "react-icons/ai";
-import { CgFileDocument } from "react-icons/cg";
+import React, { useState, useEffect } from 'react';
+import { Navbar, Container, Nav } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import { AiOutlineHome, AiOutlineUser } from 'react-icons/ai';
+import logo from '../Assets/logo.png'; // Adjust the path as necessary
 
 function NavBar() {
   const [expand, setExpand] = useState(false);
@@ -42,16 +39,6 @@ function NavBar() {
             <Nav.Item>
               <Nav.Link as={Link} to="/about" onClick={() => setExpand(false)}>
                 <AiOutlineUser style={{ marginBottom: "2px" }} /> About
-              </Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-              <Nav.Link as={Link} to="/project" onClick={() => setExpand(false)}>
-                <AiOutlineFundProjectionScreen style={{ marginBottom: "2px" }} /> Experiences
-              </Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-              <Nav.Link as={Link} to="/resume" onClick={() => setExpand(false)}>
-                <CgFileDocument style={{ marginBottom: "2px" }} /> Resume
               </Nav.Link>
             </Nav.Item>
           </Nav>
