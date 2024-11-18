c7e18d3e95adb289fcab4e2187342a99e4d4aa27
Abhinav Alok <abhinavalok318@gmail.com>
2024-11-18 11:38:27 +0530

    name change

--- src/components/Exp/Exp.js
+++ src/components/Exp/Exp.js
@@ -1,15 +1,12 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import ProjectCard from "./ProjectCards";
+// import ExpCards from "./ExpCards";
 import Particle from "../Particle";
-import leaf from "../../Assets/Projects/leaf.png";
-import emotion from "../../Assets/Projects/emotion.png";
-import editor from "../../Assets/Projects/codeEditor.png";
+import ExpCards from "./ExpCards";
 import chatify from "../../Assets/Projects/chatify.png";
-import suicide from "../../Assets/Projects/suicide.png";
 import bitsOfCode from "../../Assets/Projects/blog.png";
 
-function Projects() {
+function Exp() {
   return (
     <Container fluid className="project-section">
       <Particle />
@@ -22,7 +19,7 @@ function Projects() {
         </p>
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
           <Col md={4} className="project-card">
-            <ProjectCard
+            <ExpCards
               imgPath={chatify}
               isBlog={false}
               title="An intern in Jain Divya & CO."
@@ -31,7 +28,7 @@ function Projects() {
           </Col>
 
           <Col md={4} className="project-card">
-            <ProjectCard
+            <ExpCards
               imgPath={bitsOfCode}
               isBlog={false}
               title="Volunteer in Glad Bharat Foundation"
@@ -44,4 +41,4 @@ function Projects() {
   );
 }
 
-export default Projects;
+export default Exp;
